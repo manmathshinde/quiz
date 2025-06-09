baa06af0892deceb278be49975f6fb79fe9612b3
Shinde <manmath.shinde@accenture.com>
2025-06-09 19:23:20 +0530

    refactored the code.

--- script.js
+++ script.js
@@ -39,23 +39,27 @@ function loadQuestions() {
     loadRandomWords(level);
 }
 
+// function removeLearnedQuestions() {
+
+//     learnedQuestions.forEach((element) => {
+//         let index = questions.findIndex(q => q.question === element);
+//         questions.splice(index, 1);
+//     });
+
+// }
 function removeLearnedQuestions() {
-
-    learnedQuestions.forEach((element) => {
-        let index = questions.findIndex(q => q.question === element);
-        questions.splice(index, 1);
-    });
-
+    questions = questions.filter(q => !learnedQuestions.includes(q.question));
 }
 
 function loadRandomWords(level) {
 
     if (level == 1) {
         randomWords = randomWords_1;
-    } else if (level = 2) {
+    } else if (level == 2) {
         randomWords = randomWords_2;
     }
 }
+
 
 function loadProgress() {
     const storedProgress = JSON.parse(localStorage.getItem('progress')) || {};
@@ -184,7 +188,12 @@ function checkAnswer() {
     const question = questions[currentQuestionIndex];
     const correctAnswer = question.options;
     if (JSON.stringify(selectedOptions) === JSON.stringify(correctAnswer)) {
+        // if (!isMuted) {
+        //     audio.play();
+        // }
         if (!isMuted) {
+            audio.pause();
+            audio.currentTime = 0;
             audio.play();
         }
         currentScore++;
@@ -193,7 +202,7 @@ function checkAnswer() {
             learnedQuestions.push(question.question);
             questions.splice(currentQuestionIndex, 1);
         }
-        
+
         saveProgress();
         saveScore();
         showFeedback(true);
@@ -208,7 +217,7 @@ function checkAnswer() {
             lifelines = 5;
 
         }
-        
+
         showFeedback(false);
     }
     speakQuestion(question.answer);
@@ -271,7 +280,7 @@ function shuffleArray(array) {
 }
 
 function getVoice() {
-    window.speechSynthesis.onvoiceschanged = function() {
+    window.speechSynthesis.onvoiceschanged = function () {
         voices = window.speechSynthesis.getVoices();
         for (let i = 0; i < voices.length; i++) {
             //if (voices[i].name.includes('Pablo')) {
@@ -282,13 +291,13 @@ function getVoice() {
         }
     };
 }
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('mute-button').innerText = isMuted ? 'Unmute' : 'Mute';
     checkSession()
     loadQuestions()
     getVoice();
 });
-document.getElementById('answer').addEventListener('click', function() {
+document.getElementById('answer').addEventListener('click', function () {
     speakQuestion(this.innerHTML)
     this.classList.toggle('revealed');
 });
@@ -307,7 +316,7 @@ function checkSession() {
     const now = new Date().getTime();
     const oneDay = 24 * 60 * 60 * 1000; // One day in milliseconds
 
-    if (lastVisit ===null || (lastVisit && (now - lastVisit > oneDay))) {
+    if (lastVisit === null || (lastVisit && (now - lastVisit > oneDay))) {
         // Redirect to index.html if the last visit was more than a day ago
         window.location.href = 'index.html';
     } else {
